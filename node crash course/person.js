6167812ce51e4806f1855c3e599870595419bff5
nahuelontech <87518314+nahuelontech@users.noreply.github.com>
2021-09-13 14:46:59 +0200

    Update person.js

--- node crash course/person.js
+++ node crash course/person.js
@@ -11,9 +11,11 @@ class Person {
     this.age = age;
   }
 
-  greeting() {
+  greeting() { 
     console.log(`My name is ${this.name} and I am ${this.age}`);
-  }
+  }// Una vez escrito esto pues podemos exportar este js a otro js y poner las variables coomo x ej
+    // const person1 = new Person('John Doe', 30);(no esta escrito x los otros docs, xk son cosas sencillas)
+    //person1.greeting()     abajo
 }
 
 module.exports = Person;
